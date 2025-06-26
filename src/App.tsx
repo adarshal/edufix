642edf9449a93917c5d29609ec9b80046d7d2093
Adarsh Alagade <adarsh@hoc.com>
2025-06-26 23:17:09 +0530

    add git workflow

--- src/App.tsx
+++ src/App.tsx
@@ -104,9 +104,9 @@ function App(): JSX.Element {
                   <AboutSection />
                   <CoursesCarousel />
                   <MetricsSection />
-                  <TestimonialsCarousel />
-                  <SuccessStories />
                   <TeamGrid />
+                  <SuccessStories />
+                  <TestimonialsCarousel />
                   <Footer />
                   <FloatingButtons 
                     onDemoClick={() => {
